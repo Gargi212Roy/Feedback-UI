02383f9b2745cc62f1e310953dcfc572000a54b5
itsgargiroy <itsgargiroy@gmail.com>
2023-02-03 21:32:57 +0530

    sixth commit

---
+++ build/precache-manifest.da29fdcfce2ea8beb6676684565981db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "de767d10b0a7d3544ed6",
+    "url": "/static/css/main.d56d5179.chunk.css"
+  },
+  {
+    "revision": "de767d10b0a7d3544ed6",
+    "url": "/static/js/main.de767d10.chunk.js"
+  },
+  {
+    "revision": "773b4f539c9e9dd6bac8",
+    "url": "/static/js/1.773b4f53.chunk.js"
+  },
+  {
+    "revision": "5bfc518b6aeb2c846444",
+    "url": "/static/js/2.5bfc518b.chunk.js"
+  },
+  {
+    "revision": "3dfc8b6125724d8703c8",
+    "url": "/static/js/runtime~main.3dfc8b61.js"
+  },
+  {
+    "revision": "c70cf190817eca11cda3c049020d5aef",
+    "url": "/index.html"
+  }
+];
