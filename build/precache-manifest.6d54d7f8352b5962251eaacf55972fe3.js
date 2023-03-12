3a16e81d7ecd5c30dfd8f5c2241fbdcef4a99473
itsgargiroy <itsgargiroy@gmail.com>
2023-03-12 12:50:26 +0530

    final touches

---
+++ build/precache-manifest.6d54d7f8352b5962251eaacf55972fe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "19cf8bd56670d4d821ff",
+    "url": "/static/css/main.2dad2c9c.chunk.css"
+  },
+  {
+    "revision": "19cf8bd56670d4d821ff",
+    "url": "/static/js/main.19cf8bd5.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "be755ccb14110d67393e",
+    "url": "/static/js/2.be755ccb.chunk.js"
+  },
+  {
+    "revision": "b3d155d33fc8f3126447",
+    "url": "/static/js/runtime~main.b3d155d3.js"
+  },
+  {
+    "revision": "4ba612482debeec96808b5fd52c12534",
+    "url": "/static/media/spinner.4ba61248.gif"
+  },
+  {
+    "revision": "3deb0111dfc2568033faf103afefa983",
+    "url": "/index.html"
+  }
+];
